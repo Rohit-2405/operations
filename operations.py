eb2745dc1d175f008293ed9d6beaa64b63af92fe
Rohit-2405 <trohitrao2405@gmail.com>
2025-06-28 14:06:01 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,10 @@
+n_1=int(input("enter the first number:"))
+n_2=int(input("enter the second number:"))
+addition=n_1+n_2
+substraction=n_1-n_2
+multiplication=n_1*n_2 
+division=n_1/n_2
+print(addition)
+print(substraction)
+print(multiplication)
+print(division)
